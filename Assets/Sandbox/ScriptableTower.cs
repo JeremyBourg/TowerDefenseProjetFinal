dd1a2389e302f6bdc6da717c1689257e4a5d1cc9
Nicholas5426 <Nicholas.Surprenant2005@gmail.com>
2024-12-04 16:11:16 -0500

    Ajout Scriptable

---
+++ Assets/Sandbox/ScriptableTower.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Tower_name",menuName="Add Tower Data/Tower")]
+public class ScriptableTower : ScriptableObject
+{
+    public float nbPointsVies;
+    public float nbPointsDommages;
+    public float tempsRecharge;
+    public float vitesseDeplacement;
+    public float vitesseMissile;
+  
+}
